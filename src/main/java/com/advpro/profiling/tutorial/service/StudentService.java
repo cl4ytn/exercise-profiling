b06e356d8cee78a3744cb12ea12e04f0109d026d
cyll0 <c.nagle744@gmail.com>
2024-03-14 00:11:33 +0700

    [Refactor] Optimize findStudentWithHighestGpa

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.Comparator;
 
 /**
  * @author muhammad.khadafi
@@ -28,16 +29,7 @@ public class StudentService {
     }
 
     public Optional<Student> findStudentWithHighestGpa() {
-        List<Student> students = studentRepository.findAll();
-        Student highestGpaStudent = null;
-        double highestGpa = 0.0;
-        for (Student student : students) {
-            if (student.getGpa() > highestGpa) {
-                highestGpa = student.getGpa();
-                highestGpaStudent = student;
-            }
-        }
-        return Optional.ofNullable(highestGpaStudent);
+        return Optional.ofNullable(studentRepository.findFirstByOrderByGpaDesc());
     }
 
     public String joinStudentNames() {
